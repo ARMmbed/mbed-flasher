93e7f323d569bc386929d21c9db6334ecadf531e
Veli-Matti Lahtela <veli-matti.lahtela@arm.com>
2016-08-24 09:18:38 +0300

    rest of the licenses updated.

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 """
-This module defines the attributes of the
-PyPI package for the mbed SDK test suite
-"""
-
-"""
-mbed SDK
-Copyright (c) 2011-2015 ARM Limited
+Copyright 2016 ARM Limited
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,8 +12,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
-Author: Jussi Vatjus-Anttila <jussi.vatjus-anttila@arm.com>
 """
 
 import os
