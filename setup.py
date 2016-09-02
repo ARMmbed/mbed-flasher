de2504fe1ca51f1d90b3da09ab88c0204fb55723
Veli-Matti Lahtela <veli-matti.lahtela@arm.com>
2016-09-02 11:01:29 +0300

    version increase

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='mbed-flasher',
-      version='0.3.1',
+      version='0.4.0',
       description=DESCRIPTION,
       long_description=read('README.md'),
       author=OWNER_NAMES,
