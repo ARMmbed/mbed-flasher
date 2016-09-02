507d52a5005a8ea3520ec433c1728abdd087288c
Veli-Matti Lahtela <veli-matti.lahtela@arm.com>
2016-09-02 14:37:59 +0300

    unittest fix and notation of devices verified

--- test/test_main.py
+++ test/test_main.py
@@ -23,7 +23,7 @@ import mock
 from mbed_flasher.main import FlasherCLI
 from StringIO import StringIO
 
-flasher_version = '0.3.1'
+flasher_version = '0.4.0'
 
 
 class MainTestCase(unittest.TestCase):
