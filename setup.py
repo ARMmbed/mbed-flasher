4ff84b3abd1d7ce488a8c13fbfb990d6d440399a
Jussi Vatjus-Anttila <jussiva@gmail.com>
2018-01-23 11:30:34 +0200

    update mbed-ls to 1.3.6 (#94)

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='mbed-flasher',
           "console_scripts": ["mbedflash=mbed_flasher:mbedflash_main",],
       },
       install_requires=[
-          "mbed-ls==1.3.5",
+          "mbed-ls==1.3.6",
           "six",
           "pyserial",
           "pyOCD"
