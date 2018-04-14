8aed1dc7dd4ae56a3515eb78a92efea65784108a
Jussi Vatjus-Anttila <jussiva@gmail.com>
2018-04-14 08:26:01 +0300

    update mbedls (#116)

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name="mbed-flasher",
           "console_scripts": ["mbedflash=mbed_flasher:mbedflash_main",],
       },
       install_requires=[
-          "mbed-ls==1.4.1",
+          "mbed-ls==1.4.2",
           "six",
           "pyserial",
           "pyOCD"
