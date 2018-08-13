dc42337a555d406cd26422401aa63684ce81c119
Jussi Vatjus-Anttila <jussi.vatjus-anttila@arm.com>
2018-08-13 13:18:46 +0300

    Update version number for next release (#137)

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ LICENSE = open("LICENSE").read()
 DESCRIPTION = "mbed-flasher"
 OWNER_NAMES = "Jussi Vatjus-Anttila"
 OWNER_EMAILS = "Jussi.Vatjus-Anttila@arm.com"
-VERSION = "0.8.2"
+VERSION = "0.9.0"
 
 # Utility function to cat in a file (used for the README)
 def read(fname):
