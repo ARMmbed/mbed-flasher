bb99207b2168fd6ce888fedbf5bc5c4322b9dd60
Juho Hovila <juho.hovila@arm.com>
2019-03-01 12:50:23 +0200

    Hardcode mbedls to v1.7.2.

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name="mbed-flasher",
           "console_scripts": ["mbedflash=mbed_flasher:mbedflash_main",],
       },
       install_requires=[
-          "mbed-ls>=1.5.1,==1.*",
+          "mbed-ls==1.7.2",
           "six==1.*",
           "pyserial==3.*"
       ],
